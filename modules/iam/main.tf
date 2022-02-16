52542853e8dcc35cc90f4ae9cdea6fb269a86b62
MOHAMED DOUNNANI <dounnani@gmail.com>
2022-02-16 15:33:57 +0100

    chore: linting

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,20 +1,20 @@
 resource "aws_iam_role_policy" "DynamoDB-Policy" {
-  count                                                 = 0
-  name                                                  = "${aws_iam_role.lambda-role.name}-Policy"
-  role                                                  = aws_iam_role.lambda-role.id
-  policy                                                = jsonencode(
-                                                            {
-                                                              "Version": "2012-10-17",
-                                                              "Statement": [
-                                                                {
-                                                                  "Sid": "DynamoDBExecutions",
-                                                                  "Effect": "Allow",
-                                                                  "Action": "${var.dynamodb_policy_action_list}",
-                                                                  "Resource": "*"
-                                                                }
-                                                              ]
-                                                            }
-                                                          )
+  count = 0
+  name = "${aws_iam_role.lambda-role.name}-Policy"
+  role = aws_iam_role.lambda-role.id
+  policy = jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "DynamoDBExecutions",
+          "Effect": "Allow",
+          "Action": "${var.dynamodb_policy_action_list}",
+          "Resource": "*"
+        }
+      ]
+    }
+  )
 }
 
 
@@ -24,35 +24,35 @@ resource "aws_iam_role" "lambda-role" {
 }
 
 resource "aws_iam_role_policy" "Layer-Policy" {
-  name                                                  = "${aws_iam_role.lambda-role.name}-Layer-Policy"
-  role                                                  = aws_iam_role.lambda-role.id
-  policy                                                = jsonencode(
-                                                            {
-                                                              "Version": "2012-10-17",
-                                                              "Statement": [
-                                                                {
-                                                                  "Sid": "DynamoDBExecutions",
-                                                                  "Effect": "Allow",
-                                                                  "Action": "${var.dynamodb_policy_action_list}",
-                                                                  "Resource": "*"
-                                                                }
-                                                              ]
-                                                            }
-                                                        )
+  name = "${aws_iam_role.lambda-role.name}-Layer-Policy"
+  role = aws_iam_role.lambda-role.id
+  policy = jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "DynamoDBExecutions",
+          "Effect": "Allow",
+          "Action": "${var.dynamodb_policy_action_list}",
+          "Resource": "*"
+        }
+      ]
+    }
+  )
 }
 
 
 resource "aws_iam_role" "apigw-role" {
-  name                                                  = "${var.api_gw_name}-Role"
-  assume_role_policy                                    = file("${path.module}/apigw-role.json")
+  name = "${var.api_gw_name}-Role"
+  assume_role_policy = file("${path.module}/apigw-role.json")
 }
 
 resource "aws_iam_role_policy_attachment" "Lambda-CloudWatch-Logs-ReadWrite" {
-  policy_arn                                            = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
-  role                                                  = aws_iam_role.lambda-role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+  role = aws_iam_role.lambda-role.name
 }
 
 resource "aws_iam_role_policy_attachment" "API-GW-CloudWatch-Logs-ReadWrite" {
-  policy_arn                                            = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
-  role                                                  = aws_iam_role.apigw-role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+  role = aws_iam_role.apigw-role.name
 }
